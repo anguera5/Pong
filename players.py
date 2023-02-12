94f4f83f3546b5597c55dffd9b76b885389736de
Albert Anguera <albert.anguera.sempere@gmail.com>
2023-02-12 16:13:16 +0100

    Adding players class to be an actual player or opponent

---
+++ players.py
@@ -0,0 +1,36 @@
+from conf import SCREEN_WIDTH, SCREEN_HEIGHT
+from block import Block
+
+
+class Opponent(Block):
+    def __init__(self, path, x_pos, y_pos, speed):
+        super().__init__(path, x_pos, y_pos)
+        self.speed = speed
+
+    def update(self, ball_group):
+        if self.rect.top < ball_group.sprite.rect.y:
+            self.rect.y += self.speed
+        if self.rect.bottom > ball_group.sprite.rect.y:
+            self.rect.y -= self.speed
+        self.constrain()
+
+    def constrain(self):
+        if self.rect.top <= 0: self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT: self.rect.bottom = SCREEN_HEIGHT
+        
+
+class Player(Block):
+    def __init__(self, path, x_pos, y_pos, speed):
+        super().__init__(path, x_pos, y_pos)
+        self.speed = speed
+        self.movement = 0
+
+    def screen_constrain(self):
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+    def update(self, ball_group):
+        self.rect.y += self.movement
+        self.screen_constrain()
